7f2f0848d240fca2d979508fb79b21f7581f6d72
123e456y78 <137395377+123e456y78@users.noreply.github.com>
2024-09-12 22:25:06 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,10 +1,16 @@
 my_dict = {'Илья': 2004, 'Вика': 2001}
+print('Словарь:', my_dict)
+print('Год рождения Илья:', my_dict['Илья'])
+print('Год рождения Маша:', my_dict.get('Маша', 'нет такого ключа'))
 my_dict['Маша'] = 2004
 my_dict.get ('Саша', 2006)
-my_dict.pop ('Вика')
-print(my_dict)
-
+removed_year = my_dict.pop('Вика')
+print('Значение удалённого элемента \'Вика\':', removed_year)
+print('Изменённый словарь:', my_dict)
+print()
 my_set = {23,23, 'Арбуз','Арбуз', 4.13, 4.13}
-my_set.update ({'Малина', 36})
-print(my_set.remove (4.13))
-print(my_set)
+print('Множество:', my_set)
+my_set.add('Малина')
+my_set.add(36)
+my_set.remove (4.13)
+print('Изменённое множество:', my_set)
