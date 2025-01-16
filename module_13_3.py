435fc5b2b839b7bf32da1f926f346be6888d0c1e
123e456y78 <137395377+123e456y78@users.noreply.github.com>
2025-01-16 20:34:09 +0300

    Update module_13_3.py

--- module_13_3.py
+++ module_13_3.py
@@ -10,14 +10,14 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: types.Message):
     await message.answer('Привет! Я бот, помогающий твоему здоровью.')
-    print('Привет! Я бот, помогающий твоему здоровью.')
+    # print('Привет! Я бот, помогающий твоему здоровью.')
 
 @dp.message()
 async def all_massages(message: types.Message):
     text = message.text
     if text in ['Привет', 'привет', 'hi', 'hello']:
         await message.answer('Введите команду /start, чтобы начать общение.')
-        print('Введите команду /start, чтобы начать общение.')
+        # print('Введите команду /start, чтобы начать общение.')
     else:
         await message.answer(message.text)
 async def main():
